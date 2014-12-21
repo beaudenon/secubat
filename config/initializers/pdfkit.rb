22e6cccaa944ac53c0b0c8f010951c0e9e195f58
beaudenon <benoit.beaudenon@gmail.com>
2014-12-21 01:45:07 +0100

    Test Layout PDF

--- config/initializers/pdfkit.rb
+++
@@ -1,9 +0,0 @@
-PDFKit.configure do |config|
-  config.default_options = {
-      :page_size     => 'Letter',
-      :margin_top    => '0.5in',
-      :margin_right  => '0.5in',
-      :margin_bottom => '0.7in',
-      :margin_left   => '0.2in'
-  }
-end
